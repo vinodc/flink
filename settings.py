e42267f4a51ee1e0144990e58200bde7d43f21f8
ebaum <eugenebaumstein@gmail.com>
2011-03-06 12:47:34 -0800

    ROOT_URLCONF now more lousely coupled

--- settings.py
+++ settings.py
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = [
     'debug_toolbar.middleware.DebugToolbarMiddleware', #for debugging
 ]
 
-ROOT_URLCONF = 'flink.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_ROOT, 'templates'),
